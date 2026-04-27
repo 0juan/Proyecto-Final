cdb4e23f507a092684d2e18e89b61161e515a3d9
marianavampira6485-oss <marianavampira6485@gmail.com>
2026-04-27 17:11:11 -0500

    correccion errores

--- MundialV1/src/co/edu/unbosque/mapper/PartidoMapper.java
+++ MundialV1/src/co/edu/unbosque/mapper/PartidoMapper.java
@@ -1,23 +1,26 @@
 package co.edu.unbosque.mapper;
+
 import co.edu.unbosque.model.*;
 import co.edu.unbosque.model.Partido;
 import co.edu.unbosque.record.*;
 
-
 public class PartidoMapper {
-	public static Partido toRecord(Partido p) {
+	public static Partido toEntity(Partido p) {
 		if (p == null)
 			return null;
-	
-		return new PartidoRecord  (p.getFechaPartido(), p.isEstadoPartido(),p.getTipoDePatido(), p.getFase(),
-				p.getMarcadorLocal(), p.getMarcadorVisitante(), p.getLocalId());
+
+		return new Partido(p.getEquipoLocalId(), p.getEquipoVisitanteId(), p.getFechaPartido(),
+				p.isEstadoPartido(), p.getTipoDePatido(), p.getFase(), p.getMarcadorLocal(), p.getMarcadorVisitante(),
+				p.getLocalId());
+
 	}
-	public static Partido toEntity(PartidoRecord e) {
+
+	public static Partido toRecord(PartidoRecord e) {
 		if (e == null)
 			return null;
-	
-		return new PartidoRecord(e.setFechaPartido(), e.setEstadoPartido(), e.setTipoDePatido(), e.setFase(),
-				e.setMarcadorLocal(), e.setMarcadorVisitante(), e.setLocalId());
-		
-		return p;
+		return new Partido(	e.getEquipoLocalId(), e.getEquipoVisitanteId(), e.getFechaPartido(),
+				e.isEstadoPartido(), e.getTipoDePatido(), e.getFase(), e.getMarcadorLocal(), e.getMarcadorVisitante(),
+				e.getLocalId());
+
 	}
+}
