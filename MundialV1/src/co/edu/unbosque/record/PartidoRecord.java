2585f3c18ca07b92b856342c0b6116b81e0bfb82
marianavampira6485-oss <marianavampira6485@gmail.com>
2026-04-28 00:07:05 -0500

    record corregido

--- MundialV1/src/co/edu/unbosque/record/PartidoRecord.java
+++ MundialV1/src/co/edu/unbosque/record/PartidoRecord.java
@@ -3,101 +3,10 @@ package co.edu.unbosque.record;
 import java.time.LocalDate;
 
 public class PartidoRecord {
-	private String equipoLocalId;
-	private String equipoVisitanteId;
-	private LocalDate fechaPartido;
-	private boolean estadoPartido;
-	private String tipoDePatido;
-	private String fase;
-	private int marcadorLocal;
-	private int marcadorVisitante;
-	private int localId;
+	
 
 	public void Partido(String equipoLocalId, String equipoVisitanteId, LocalDate fechaPartido, boolean estadoPartido,
 			String tipoDePatido, String fase, int marcadorLocal, int marcadorVisitante, int localId) {
 
-		this.equipoLocalId = equipoLocalId;
-		this.equipoVisitanteId = equipoVisitanteId;
-		this.fechaPartido = fechaPartido;
-		this.estadoPartido = estadoPartido;
-		this.tipoDePatido = tipoDePatido;
-		this.fase = fase;
-		this.marcadorLocal = marcadorLocal;
-		this.marcadorVisitante = marcadorVisitante;
-		this.localId = localId;
-
 	}
-
-	public String getEquipoLocalId() {
-		return equipoLocalId;
-	}
-
-	public void setEquipoLocalId(String equipoLocalId) {
-		this.equipoLocalId = equipoLocalId;
-	}
-
-	public String getEquipoVisitanteId() {
-		return equipoVisitanteId;
-	}
-
-	public void setEquipoVisitanteId(String equipoVisitanteId) {
-		this.equipoVisitanteId = equipoVisitanteId;
-	}
-
-	public LocalDate getFechaPartido() {
-		return fechaPartido;
-	}
-
-	public void setFechaPartido(LocalDate fechaPartido) {
-		this.fechaPartido = fechaPartido;
-	}
-
-	public boolean isEstadoPartido() {
-		return estadoPartido;
-	}
-
-	public void setEstadoPartido(boolean estadoPartido) {
-		this.estadoPartido = estadoPartido;
-	}
-
-	public String getTipoDePatido() {
-		return tipoDePatido;
-	}
-
-	public void setTipoDePatido(String tipoDePatido) {
-		this.tipoDePatido = tipoDePatido;
-	}
-
-	public String getFase() {
-		return fase;
-	}
-
-	public void setFase(String fase) {
-		this.fase = fase;
-	}
-
-	public int getMarcadorLocal() {
-		return marcadorLocal;
-	}
-
-	public void setMarcadorLocal(int marcadorLocal) {
-		this.marcadorLocal = marcadorLocal;
-	}
-
-	public int getMarcadorVisitante() {
-		return marcadorVisitante;
-	}
-
-	public void setMarcadorVisitante(int marcadorVisitante) {
-		this.marcadorVisitante = marcadorVisitante;
-	}
-
-	public int getLocalId() {
-		return localId;
-	}
-
-	public void setLocalId(int localId) {
-		this.localId = localId;
-	}
-
 }
