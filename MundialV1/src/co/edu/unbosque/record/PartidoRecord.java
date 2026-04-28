4814d060c0aae652db78f1584b744cf9779678c8
marianavampira6485-oss <marianavampira6485@gmail.com>
2026-04-28 11:30:13 -0500

    record modo

--- MundialV1/src/co/edu/unbosque/record/PartidoRecord.java
+++ MundialV1/src/co/edu/unbosque/record/PartidoRecord.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class PartidoRecord {
 	
 
-	public void Partido(String equipoLocalId, String equipoVisitanteId, LocalDate fechaPartido, boolean estadoPartido,
+	public record Partidorecord (String equipoLocalId, String equipoVisitanteId, LocalDate fechaPartido, boolean estadoPartido,
 			String tipoDePatido, String fase, int marcadorLocal, int marcadorVisitante, int localId) {
 
 	}
